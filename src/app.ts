bb35310b9b80284e360c78d7dcd2ac8a09a4332f
Raj Rai <raj.rai@gammaedge.io>
2026-01-20 18:37:02 +0530

    Created a express server

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app= express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello from server");
+})
+
+app.listen(3000,()=>{
+    try{
+        console.log("server created succesfully");
+    }catch(err){
+        console.log(err);
+    }
+})
